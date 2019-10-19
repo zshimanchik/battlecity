15306443b6d8e0c3cef3c1ae2f4b40b78a571945
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2019-10-19 22:43:44 +0200

    socket reconnect

--- battlecityplayer/main.py
+++ battlecityplayer/main.py
@@ -8,11 +8,17 @@ except Exception:
     from unittest.mock import Mock
     visualizer = Mock()
 
-try:
-    url = 'ws://dojorena.io/codenjoy-contest/ws?user=1nvdes0hwvkkoh3dfa21&code=1080829370632063683&gameName=battlecity'
-    player = Player(visualizer)
-    ws = BattleCityConnection(url, player)
-    ws.connect()
-    ws.run_forever()
-except KeyboardInterrupt:
-    ws.close()
+while True:
+    try:
+        url = 'ws://dojorena.io/codenjoy-contest/ws?user=1nvdes0hwvkkoh3dfa21&code=1080829370632063683&gameName=battlecity'
+        player = Player(visualizer)
+        ws = BattleCityConnection(url, player)
+        ws.connect()
+        ws.run_forever()
+    except Exception as ex:
+        print(ex)
+    finally:
+        try:
+            ws.close()
+        except Exception as ex:
+            print(ex)

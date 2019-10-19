8becc12cb5976822c07e1e1cdad87b311e7947ee
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2019-10-19 12:03:51 +0200

    a star improved

--- battlecityplayer/tactics/astar.py
+++ battlecityplayer/tactics/astar.py
@@ -1,59 +1,78 @@
 from collections import deque
 
+import player
+from constants import *
 from models import Point, Direction
 from tactics import Tactics
-from constants import *
 
 
 class AStar(Tactics):
     def update(self, player):
-        self.usability = 0
+        self.usability = 0.0
         self.action = ''
         board = player.board
-        path = self.find_path_to_enemy(board, Point(20, 5))
+        enemies = board.get_all_enemies()
+
+        path = self.find_path_to_enemy(board, Point(17, 32))
         if path and len(path) > 1:
             print(path)
             delta = path[1] - player.board.me.pos
             print(f'delta: {path[1]} - {player.board.me.pos} = {delta}')
             print('dir: ', delta.give_direction())
+            self._draw_path(path, player.visualizer)
+            ch = board.char(path[1].x, path[1].y)
             self.action = delta.give_direction().value
-            self.usability = 1
+            if ch in CONSTRUCTIONS:
+                self.action += ',act'
+            self.usability = 0.0
+
+    def _draw_path(self, path, visualizer):
+        if len(path) > 1:
+            ipath = iter(path)
+            p1 = next(ipath)
+            for p2 in ipath:
+                visualizer.client.draw_line(p1.x, p1.y, p2.x, p2.y)
+                p1 = p2
 
     def h(self, start, target):
         return (target - start).length()
 
-
     def find_path_to_enemy(self, board, target):
         INF = 999999
-        g_dist_mtrx = [[INF] * board.n for _ in range(board.n)]
-        f_dist_mtrx = [[INF] * board.n for _ in range(board.n)]
+        actual_dist_mtrx = [[INF] * board.n for _ in range(board.n)]
+        potential_dist_mtrx = [[INF] * board.n for _ in range(board.n)]
         visited = set()
         queue = deque()
         queue.appendleft(board.me.pos)
-        g_dist_mtrx[board.me.pos.x][board.me.pos.y] = 0
-        f_dist_mtrx[board.me.pos.x][board.me.pos.y] = self.h(board.me.pos, target)
+        actual_dist_mtrx[board.me.pos.x][board.me.pos.y] = 0
+        potential_dist_mtrx[board.me.pos.x][board.me.pos.y] = self.h(board.me.pos, target)
 
         while queue:
-            cur_pos = min(queue, key=lambda cur: f_dist_mtrx[cur.x][cur.y])
+            cur_pos = min(queue, key=lambda cur: potential_dist_mtrx[cur.x][cur.y])
             if cur_pos == target:
-                return self._restore_path(cur_pos, g_dist_mtrx)
+                return self._restore_path(cur_pos, actual_dist_mtrx)
 
             queue.remove(cur_pos)
             visited.add(cur_pos)
 
             for dir in Direction:
                 next_pos = cur_pos + dir.get_delta()
-                if next_pos not in visited and 0 <= next_pos.x < board.n and 0 <= next_pos.y < board.n:
+                if next_pos not in visited and board.is_belong(next_pos):
                     ch = board.char(next_pos.x, next_pos.y)
                     if ch == WALL:
                         visited.add(next_pos)
                         continue
-                    dist_to_next = g_dist_mtrx[cur_pos.x][cur_pos.y] + 1 # todo consider wall
-                    if dist_to_next < g_dist_mtrx[next_pos.x][next_pos.y]:
-                        g_dist_mtrx[next_pos.x][next_pos.y] = dist_to_next
-                        f_dist_mtrx[next_pos.x][next_pos.y] = dist_to_next + self.h(next_pos, target)
+                    dist_to_next = actual_dist_mtrx[cur_pos.x][cur_pos.y] + self._d(ch)
+                    if dist_to_next < actual_dist_mtrx[next_pos.x][next_pos.y]:
+                        actual_dist_mtrx[next_pos.x][next_pos.y] = dist_to_next
+                        potential_dist_mtrx[next_pos.x][next_pos.y] = dist_to_next + self.h(next_pos, target)
                         if next_pos not in queue:
                             queue.append(next_pos)
+
+    def _d(self, ch):
+        if ch in CONSTRUCTIONS:
+            return SHOTS_TO_DESTROY[ch] * player.Player.FIRE_COUNTDOWN
+        return 1
 
     def _restore_path(self, cur_pos, dist_mtrx):
         path = deque()

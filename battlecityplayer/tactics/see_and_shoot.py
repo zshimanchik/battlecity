e35ff788f2fe2f4339eb63b1a1cabdcd6d857980
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2019-10-19 17:25:02 +0200

    see and shoot only in the least dangerous direction

--- battlecityplayer/tactics/see_and_shoot.py
+++ battlecityplayer/tactics/see_and_shoot.py
@@ -11,20 +11,31 @@ class SeeAndShoot(Tactics):
         if player.fire_countdown != 0:
             return
         board = player.board
-        me = board.me
-        enemies_dir_and_dist = []
-        for dir in Direction:
-            view = board.get_view(Vec(me.pos, dir), True)
-            print(f'{dir} {view!r}')
-            for i, ch in enumerate(view):
-                if ch in ENEMIES:
-                    enemies_dir_and_dist.append((dir, i))
+
+        enemies_dir_and_dist = self.get_enemies_dir_and_dist(board)
+
         if not enemies_dir_and_dist:
             return
         print('Enemy dists:', enemies_dir_and_dist)
-        enemy_dir, enemy_dist = min(enemies_dir_and_dist, key=lambda x: x[1])
-        self.action = 'act,' + enemy_dir.value
-        if enemy_dist < 6:
+
+        enemies_with_3d = [(player._estimate_action(dir.value), dist, dir) for dir, dist in enemies_dir_and_dist]
+        player.visualizer.print('=====')
+        for danger, dist, dir in enemies_with_3d:
+            player.visualizer.print(f'enemy: {danger}, {dist}, {dir}')
+        player.visualizer.print('=====')
+
+        danger, dist, dir = min(enemies_with_3d, key=lambda x: (x[0], x[1]))
+        self.action = 'act,' + dir.value
+        if dist < 6:
             self.usability = 0.9
         else:
             self.usability = 0.8
+
+    def get_enemies_dir_and_dist(self, board):
+        enemies_dir_and_dist = []
+        for dir in Direction:
+            view = board.get_view(Vec(board.me.pos, dir))
+            for i, ch in enumerate(view):
+                if ch in ENEMIES:
+                    enemies_dir_and_dist.append((dir, i))
+        return enemies_dir_and_dist
